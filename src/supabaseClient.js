6a1ed78f37a8d8234ec8827db48084c977c058bc
nymaa7171-cloud <nymaa7171@gmail.com>
2026-05-23 13:31:06 +0800

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://ddwbxmnyvpblilbewsne.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRkd2J4bXludnBibGlsYmV3c25lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk0NzM0NzcsImV4cCI6MjA5NTA0OTQ3N30.zXp2fOzX2Qvn95GI_cODDW1I0lbTTTkOITrDSgYmesk'
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
